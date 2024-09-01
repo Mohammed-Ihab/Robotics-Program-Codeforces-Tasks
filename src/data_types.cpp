7a80e37d8ab3625549f1590e2364f40ac1ae01a7
Mohammed-Ihab <ihabelmetwally@outlook.com>
2024-09-01 14:57:46 +0300

    B- Basic data types task

---
+++ src/data_types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numInt {};
+    long long numLong{};
+    char x {};
+    float numFloat {};
+    double numDouble {};
+
+    cin >> numInt >> numLong >> x >> numFloat >> numDouble;
+
+    cout << numInt << endl;
+    cout << numLong << endl;
+    cout << x << endl;
+    cout << numFloat << endl;
+    cout << numDouble << endl;
+
+    return 0;
+}
