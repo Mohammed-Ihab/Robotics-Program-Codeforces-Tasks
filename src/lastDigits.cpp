3f62e78b0fd959ac3c36525aa38ef41634d99476
Mohammed-Ihab <ihabelmetwally@outlook.com>
2024-09-03 02:48:30 +0300

    Updating Y- Last 2 digits task

--- src/lastDigits.cpp
+++ src/lastDigits.cpp
@@ -3,10 +3,6 @@
 
 using namespace std;
 
-/*float get_last_two(float x){
-    return fmod(x, 100.0);
-}*/
-
 int main(){
 
     unsigned long long A {}, B {}, C {}, D {}, sum {}, last_two_digits {1};
@@ -23,32 +19,6 @@ int main(){
         cout << "0" << last_two_digits << endl;
     else
         cout << last_two_digits << endl;
-
-
-    /*if (sum > 100000){
-        product = A / 100000000 * B * C * D;
-        last_two_digits = get_last_two(product);
-        product = last_two_digits * 100000000;
-        cout << product << endl;
-        last_two_digits = get_last_two(product);
-        if(last_two_digits == 0)
-            cout << "00" << endl;
-        else
-            cout << last_two_digits << endl;
-
-    }else {
-        product = A * B * C * D;
-        if(product >= 100){
-            last_two_digits = get_last_two(product);
-            if(last_two_digits == 0)
-                cout << "00" << endl;
-            else
-                cout << last_two_digits << endl;
-        } else {
-            cout << product << endl;
-        }    
-
-    }*/
 
     return 0;
 }
