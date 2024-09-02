12932546b6946815ad9ff9787c45d7ab268199a6
Mohammed-Ihab <ihabelmetwally@outlook.com>
2024-09-02 14:16:44 +0300

    Fixing an issue in multiples file

--- src/multiples.cpp
+++ src/multiples.cpp
@@ -8,7 +8,7 @@ int main(){
     unsigned int A {}, B {};
     cin >> A >> B;
 
-    if(A%B == 0)
+    if(A%B == 0 || B%A == 0)
         cout << "Multiples" << endl;
     else
         cout << "No Multiples" << endl;
