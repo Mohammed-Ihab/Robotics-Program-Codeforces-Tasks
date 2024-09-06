17c0838f48d3c750d37253a15ace781c7f97342c
Mohammed-Ihab <ihabelmetwally@outlook.com>
2024-09-06 17:21:14 +0300

    T- Shape 2 task

---
+++ sheet_2_src/shape2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int line_num {};
+    cin >> line_num;
+
+    int stars_num {1 + 2 * line_num};
+
+    for(int i = 1; i <= line_num; i++){
+        for(int j = 1; j <= static_cast<int>(stars_num / 2) - (i); j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= 1 + 2 * (i - 1); k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
