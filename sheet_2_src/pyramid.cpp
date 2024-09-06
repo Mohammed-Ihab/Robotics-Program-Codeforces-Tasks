04760dc991bd5ddc1adc1bd95cda3c9bdd340dd0
Mohammed-Ihab <ihabelmetwally@outlook.com>
2024-09-06 04:00:11 +0300

    O- Pyramid task

---
+++ sheet_2_src/pyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int A {};
+    cin >> A;
+
+    for(int i = 1; i <= A; i++){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
